c7ebfad8a8eb51147b6d45cca8feab375d3a2ea9
Joshi <finn.joshua.kaup@gmail.com>
2024-09-29 14:07:00 +0200

    add patch and delete

--- routes/routes.js
+++ routes/routes.js
@@ -5,28 +5,41 @@ const uri = "mongodb+srv://finnjoshuakaup:Zo3eBFKw2J9SvA7R@listcluster.vqluu.mon
 
 
 router.get("/get", async (req, res) => {
-  let data = await collection("find");
+  const today = new Date().toISOString().substring(0, 10);
+  let data = await collection("find", { start: { $gte: today } });
   res.send(data);
-})
+});
 
 router.post("/post", async (req, res) => {
   let data = await collection("insertOne", req.body);
   res.send(data);
-  console.log(data);
-})
+});
+
+router.patch("/patch", async (req, res) => {
+  let data = await collection("updateOne", req.body[0], { $set: req.body[1]});
+  res.send(data);
+});
+
+router.delete("/delete", async (req, res) => {
+  let data = await collection("deleteOne", req.body);
+  res.send(data);
+});
 
 
-async function collection(operation, data) {
-    const client = new MongoClient(uri);
-    try {
-        if(operation === "find") {
-            const content = await client.db("calender").collection("calender")[operation](data).sort({start: 1}).toArray();
-            return content;
-        } else {
-            const content = await client.db("calender").collection("calender")[operation](data);
-            return content;
-        }
-    } finally {await client.close();}
+async function collection(operation, data, update) {
+  const client = new MongoClient(uri);
+  try {
+    if(operation === "find") {
+      const content = await client.db("calender").collection("calender")[operation](data).sort({start: 1}).toArray();
+      return content;
+    } else if(operation === "updateOne") {
+      const content = await client.db("calender").collection("calender")[operation](data, update);
+      return content;
+    } else {
+      const content = await client.db("calender").collection("calender")[operation](data);
+      return content;
+    }
+  } finally {await client.close();}
 }
 
 /*async function db() {
