f5d4c959c8080e7f458a2fcbf30e6be6d4ca4628
Joshi <finn.joshua.kaup@gmail.com>
2024-10-06 18:18:42 +0200

    change back to http

--- server.js
+++ server.js
@@ -2,17 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const router = require("./routes/routes");
-const fs = require("fs");
-const https = require('https');
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 app.use(router);
 
-const privateKey = fs.readFileSync('example.com.key');
-const certificate = fs.readFileSync('example.com.crt');
-const credentials = { key: privateKey, cert: certificate };
-
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(3000);
+app.listen(3000);
