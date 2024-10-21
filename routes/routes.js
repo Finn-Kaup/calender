d00fffcbbfa6201653ac0b3ed755a9ecb9323b52
Joshi <finn.joshua.kaup@gmail.com>
2024-10-21 20:41:03 +0200

    add birthdays

--- routes/routes.js
+++ routes/routes.js
@@ -4,54 +4,58 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://finnjoshuakaup:Zo3eBFKw2J9SvA7R@listcluster.vqluu.mongodb.net/?retryWrites=true&w=majority&appName=listCluster";
 
 
-router.get("/get", async (req, res) => {
-  const today = new Date().toISOString().substring(0, 10);
-  let data = await collection("find", { start: { $gte: today } });
+router.get("/:collection/get", async (req, res) => {
+  const date = new Date()
+  const today = date.toISOString().substring(0, 10);
+  let data;
+  switch (req.params.collection) {
+    case "calender":
+      data = await db(req.params.collection, "find", { start: { $gte: today } }, {start: 1});
+      break;
+    case "events":
+      break;
+    case "birthdays":
+      const currentMonth = date.getMonth() + 1;
+      const currentDay = date.getDate();
+      const data1 = await db(req.params.collection, "find", {month: {$gte: currentMonth}, day: {$gte: currentDay}}, {month: 1, day: 1});
+      const data2 = await db(req.params.collection, "find", {month: {$lt: currentMonth }}, {month: 1, day: 1});
+      const data3 = await db(req.params.collection, "find", {month: {$eq: currentMonth }, day: {$lt: currentDay}}, {month: 1, day: 1});
+      data = data1.concat(data2).concat(data3);
+      break;
+  }
   res.send(data);
 });
 
-router.post("/post", async (req, res) => {
-  let data = await collection("insertOne", req.body);
+router.post("/:collection/post", async (req, res) => {
+  let data = await db(req.params.collection, "insertOne", req.body);
   res.send(data);
 });
 
-router.patch("/patch", async (req, res) => {
-  let data = await collection("updateOne", req.body[0], { $set: req.body[1]});
+router.patch("/:collection/patch", async (req, res) => {
+  let data = await db(req.params.collection, "updateOne", req.body[0], { $set: req.body[1]});
   res.send(data);
 });
 
-router.delete("/delete", async (req, res) => {
-  let data = await collection("deleteOne", req.body);
+router.delete("/:collection/delete", async (req, res) => {
+  let data = await db(req.params.collection, "deleteOne", req.body);
   res.send(data);
 });
 
 
-async function collection(operation, data, update) {
+async function db(collection, operation, data, updateSort) {
   const client = new MongoClient(uri);
   try {
     if(operation === "find") {
-      const content = await client.db("calender").collection("calender")[operation](data).sort({start: 1}).toArray();
+      const content = await client.db("calender").collection(collection)[operation](data).sort(updateSort).toArray();
       return content;
     } else if(operation === "updateOne") {
-      const content = await client.db("calender").collection("calender")[operation](data, update);
+      const content = await client.db("calender").collection(collection)[operation](data, updateSort);
       return content;
     } else {
-      const content = await client.db("calender").collection("calender")[operation](data);
+      const content = await client.db("calender").collection(collection)[operation](data);
       return content;
     }
   } finally {await client.close();}
 }
-
-/*async function db() {
-    const client = new MongoClient(uri);
-    try {
-        const content = await client.db("calender").listCollections().toArray();
-        let data = []
-        for(let i = 0; i < content.length; i++) {
-          data.push(content[i].name); 
-        }
-        return data;
-      } finally {await client.close();}
-}*/
 
 module.exports = router;
